a26fba18681c2049221da77ab762178ca4d028da
votre nom <votre@email>
2017-02-12 20:26:32 +0100

    marche plus j'ai cassé quelque chose ;-)

--- demo_git_branches.sh
+++ demo_git_branches.sh
@@ -12,69 +12,92 @@ alias gtree='git log --graph --oneline --decorate --all'
 echo "1 commit"
 git init
 git add --all
-git commit -m "1 commit initial"
+git commit -m "1 commit, commit initial"
 
 echo "2 commit"
 git checkout master
 git checkout -b dev
-echo "c'est tata" > tata.txt
-echo "112710" >> toto.txt
+echo "2" >> toto.txt
+echo "2" >> tutu.txt
 git add --all
-git commit -m "2 commit, création de la branche dev et ajout de tata.txt"
+git commit -m "2 commit, création de la branche dev"
 
 echo "3 commit"
-git checkout dev
+git checkout master
 git checkout -b test
+echo "3" >> toto.txt
+echo "3" >> tutu.txt
 git add --all
 git commit -m "3 commit, création de la branche test"
 
 echo "4 commit"
-git checkout dev
+git checkout master
 git checkout -b bug1
-echo "113331" >> toto.txt
-echo "113331" >> titi.txt
+echo "4" >> toto.txt
 git add --all
-git commit -m "4 commit, création de la branche bug1 et merge de la branche test dans la bug1"
+git commit -m "4 commit, création de la branche bug1"
 
 echo "5 commit"
-git checkout dev
+git checkout master
 git checkout -b bug2
-echo "113515" >> toto.txt
-echo "113515" >> tata.txt
+echo "5" >> tutu.txt
 git add --all
-git commit -m "5 commit, création de la branche bug2 et merge de la branche test dans la bug2"
+git commit -m "5 commit, création de la branche bug2"
 
 echo "6 commit"
 git checkout bug1
-echo "113331" >> toto.txt
-echo "113331" >> titi.txt
+echo "6" >> toto.txt
 git add --all
-git commit -m "6 commit, retour dans la branche bug1, et modifé toto.txt et titi.txt""
+git commit -m "6 commit, retour dans la branche bug1, etmodifé toto.txt et tutu.txt"
 
 echo "7 commit"
 git checkout bug2
-echo "113331" >> toto.txt
-echo "113331" >> titi.txt
+echo "7" >> tutu.txt
 git add --all
-git commit -m "7 commit, retour dans la branche bug2, et modifé toto.txt et titi.txt""
+git commit -m "7 commit, retour dans la branche bug2, et modifé toto.txt et tutu.txt"
 
-echo "8 commit"
-git checkout test
-git merge bug1
-git add --all
-git commit -m "8 commit, merge de la branche bug1 dans la test"
+#echo "8 commit"
+#git checkout master
+#echo "8" >> toto.txt
+#echo "8" >> tutu.txt
+#git add --all
+#git commit -m "8 commit, retour dans la branche master, et modifé toto.txt et tutu.txt"
 
 echo "9 commit"
-git checkout test
-git merge bug2
+git checkout master
+git merge bug1
+echo "9" >> toto.txt
 git add --all
-git commit -m "9 commit, merge de la branche bug2 dans la test"
+git commit -m "9 commit, merge de la branche bug1 dans la master"
 
 echo "10 commit"
 git checkout master
-git merge test
+git merge bug2
+echo "10" >> tutu.txt
 git add --all
-git commit -m "10 commit, merge de la branche test dans la master"
+git commit -m "10 commit, merge de la branche bug2 dans la master"
+
+echo "11 commit"
+git checkout test
+echo "11" >> toto.txt
+echo "11" >> tutu.txt
+git add --all
+git commit -m "11 commit, retour dans la branche test, et modifé toto.txt et tutu.txt"
+
+echo "12 commit"
+git checkout dev
+echo "12" >> toto.txt
+echo "12" >> tutu.txt
+git add --all
+git commit -m "12 commit, retour dans la branche dev, et modifé toto.txt et tutu.txt"
+
+echo "13 commit"
+git checkout master
+echo "13" >> toto.txt
+echo "13" >> tutu.txt
+git add --all
+git commit -m "13 commit, retour dans la branche master, et modifé toto.txt et tutu.txt"
+
 
 
 
