35036aa3b4ff044d041512dd99c127983c7582ab
votre nom <votre@email>
2017-02-12 19:29:03 +0100

    Petite sauvegarde de secours de mon nouveau script de demo git

--- env_a_zuzu.sh
+++ env_a_zuzu.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #Petit script pour créer mon environnement git ;-)
-#zf170211.1211
+#zf170212.1859
 
 git config --global user.name "Christian Zufferey"
 git config --global user.email "christian@zufferey.com"
+git config --global credential.helper "cache --timeout=86400"
+
 alias gtree='git log --graph --oneline --decorate --all'
 
 git config -l
